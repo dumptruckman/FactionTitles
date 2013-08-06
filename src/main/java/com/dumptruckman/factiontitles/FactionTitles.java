1263848491553ca444a913e1c2d0eebc617e794f
Jeremy Wood <farachan@gmail.com>
2013-08-06 12:35:53 -0400

    1.0.2 - Updated code for Statboard compat.

--- src/main/java/com/dumptruckman/factiontitles/FactionTitles.java
+++ src/main/java/com/dumptruckman/factiontitles/FactionTitles.java
@@ -7,15 +7,17 @@ import com.massivecraft.factions.event.FactionsEventMembershipChange;
 import com.massivecraft.factions.event.FactionsEventNameChange;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scoreboard.Scoreboard;
 import org.bukkit.scoreboard.Team;
+
+import java.util.List;
 
 public class FactionTitles extends JavaPlugin implements Listener {
 
@@ -29,56 +31,84 @@ public class FactionTitles extends JavaPlugin implements Listener {
         getServer().getPluginManager().registerEvents(this, this);
     }
 
-    @EventHandler(priority = EventPriority.MONITOR)
+    @EventHandler(priority = EventPriority.NORMAL)
     public void playerJoin(PlayerJoinEvent event) {
-        refreshScoreboards();
-    }
+        final Player player = event.getPlayer();
 
-    @EventHandler(priority = EventPriority.MONITOR)
-    public void playerQuit(PlayerQuitEvent event) {
-        refreshScoreboards();
+        // Set up new scoreboard for player that just joined to make sure nothing weird is going on with their scoreboards
+
+        Scoreboard scoreboard = getServer().getScoreboardManager().getNewScoreboard();
+        player.setScoreboard(scoreboard);
+
+        // Set the faction names for all online players as their team suffix
+
+        String factionName = UPlayer.get(player).getFaction().getName();
+        for (Player p : Bukkit.getOnlinePlayers()) {
+            getTeam(scoreboard, p).addPlayer(p);
+            getTeam(p.getScoreboard(), player.getWorld(), factionName).addPlayer(player);
+        }
     }
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void playerJoinFaction(FactionsEventMembershipChange event) {
+        final Player player = Bukkit.getPlayerExact(event.getUPlayer().getName());
+        if (player == null) {
+            throw new IllegalStateException("Player should not be null!");
+        }
+
+        // After the player has joined the faction, refresh every ones team names for that player.
+
         Bukkit.getScheduler().runTaskLater(this, new Runnable() {
             @Override
             public void run() {
-                refreshScoreboards();
+                Scoreboard scoreboard = player.getScoreboard();
+                String factionName = UPlayer.get(player).getFaction().getName();
+                for (Player p : Bukkit.getOnlinePlayers()) {
+                    getTeam(scoreboard, p).addPlayer(p);
+                    getTeam(p.getScoreboard(), player.getWorld(), factionName).addPlayer(player);
+                }
             }
         }, 1L);
     }
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
-    public void playerJoinFaction(FactionsEventNameChange event) {
-        Bukkit.getScheduler().runTaskLater(this, new Runnable() {
-            @Override
-            public void run() {
-                refreshScoreboards();
+    public void factionNameChange(FactionsEventNameChange event) {
+        Faction faction = event.getFaction();
+        List<Player> fPlayers = faction.getOnlinePlayers();
+        String newName = event.getNewName();
+
+        // Update all players on the server with the new team name
+
+        for (Player p : Bukkit.getOnlinePlayers()) {
+            Scoreboard scoreboard = p.getScoreboard();
+            for (Player fPlayer : fPlayers) {
+                getTeam(scoreboard, fPlayer.getWorld(), newName).addPlayer(fPlayer);
             }
-        }, 1L);
+        }
     }
 
-    public void refreshScoreboards() {
-        for (Player p : Bukkit.getOnlinePlayers()) {
-            Scoreboard scoreboard = Bukkit.getScoreboardManager().getMainScoreboard();
-            p.setScoreboard(scoreboard);
-            UPlayer uPlayer = UPlayer.get(p);
-            Faction faction = uPlayer.getFaction();
-            String factionName = faction.getName();
+    /**
+     * Gets the team with appropriate suffix for the specified scoreboard and player's faction, registering it if necessary.
+     */
+    private Team getTeam(Scoreboard scoreboard, Player player) {
+        return getTeam(scoreboard, player.getWorld(), UPlayer.get(player).getFaction().getName());
+    }
 
-            Team team = scoreboard.getTeam(factionName);
-            if (team == null) {
-                team = scoreboard.registerNewTeam(factionName);
-                team.setDisplayName(factionName.substring(0, factionName.length() <= 16 ? factionName.length() : 16));
-            }
-            team.setPrefix("");
-            if (faction.equals(FactionColls.get().getForWorld(p.getWorld().getName()).getNone())) {
-                team.setSuffix("");
-            } else {
-                team.setSuffix(" - " + ChatColor.GOLD + factionName.substring(0, factionName.length() <= 11 ? factionName.length() : 11));
-            }
-            team.addPlayer(p);
+    /**
+     * Gets the team with appropriate suffix for the specified scoreboard and faction name, registering it if necessary.
+     */
+    private Team getTeam(Scoreboard scoreboard, World world, String factionName) {
+        Team team = scoreboard.getTeam(factionName);
+        if (team == null) {
+            team = scoreboard.registerNewTeam(factionName);
+            team.setDisplayName(factionName.substring(0, factionName.length() <= 16 ? factionName.length() : 16));
         }
+        team.setPrefix("");
+        if (factionName.equals(FactionColls.get().getForWorld(world.getName()).getNone().getName())) {
+            team.setSuffix("");
+        } else {
+            team.setSuffix(" - " + ChatColor.GOLD + factionName.substring(0, factionName.length() <= 11 ? factionName.length() : 11));
+        }
+        return team;
     }
 }
